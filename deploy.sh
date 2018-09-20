9593bd6bf5f1e8569f6038346471c7f9e0b4c2fa
Matias Villagra <mativillagra@gmail.com>
2018-09-20 15:03:31 -0300

    Add delivery continuo

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 
 PASS=$1
 
-container=$(docker ps -aq --filter "name=scratch.0.0.$PASS")
+container=$(docker ps -aq --filter "name=scratch")
 
 echo $container 
 
 if [[ "$container" == '' ]]; then          
 
-       docker run -d -p 8888:5000 --name scratch.0.0.$PASS scratch.0.0.$PASS
+       docker run -d -p 8888:5000 --name scratch scratch.0.0.$PASS
 
 else 
 
-       docker stop $container && docker rm $container 
+       docker stop $container && docker rm $container && docker run -d -p 8888:5000 --name scratch scratch.0.0.$PASS
 
 fi
